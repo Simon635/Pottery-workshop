e0908f522c97c5d2d475c9e807423f33237d29cd
simon <semyon.dudin@inbox.ru>
2025-11-07 19:20:37 +0300

    Правки кнопки якори

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import './toggle-faq.js'
 import './check-select.js'
 import './open-modal.js'
 import './delete.js'
+import './card-scroll.js'
 
 const swiper = new Swiper('.subscription-swiper', {
 	loop: true,
